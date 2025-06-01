8b7b4cc26ddef736ba882d76a0c5ef7134786e98
WaelAlfanan <waelbahaa01@hotmail.com>
2025-06-01 18:50:02 +0100

    Implemented on the Array
    - Implemented with the Resize and the GetAt

---
+++ Array/ResizeAndGetAll/ResizeAndGetAll/ArrayImplementations.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+namespace ResizeAndGetAll
+{
+	public static class ArrayImplementations
+	{
+		public static void ResizeArray<T>(ref T[] sourceArray, int newSize)
+		{
+			// validations
+			if (newSize <= 0) return;
+			if (sourceArray == null) return;
+			if (newSize <= sourceArray.Length) return;
+
+			// resizing process
+			T[] newArray = new T[newSize];
+
+			Buffer.BlockCopy(sourceArray, 0, newArray, 0,
+							Buffer.ByteLength(sourceArray));
+
+			sourceArray = newArray;
+		}
+
+		public static T GetAt<T>(T[] Arr, int index, int sizeOf)
+		{
+
+			if (index < 0) return default(T);
+            ref byte zeroAdrr = ref MemoryMarshal.GetArrayDataReference((Array)Arr);
+            ref byte indexRef = ref Unsafe.Add(ref zeroAdrr, index * sizeOf);
+            ref T item = ref Unsafe.As<byte, T>(ref indexRef);
+			return item;
+
+        }
+	}
+}
---
+++ Array/ResizeAndGetAll/ResizeAndGetAll/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ResizeAndGetAll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[3] { 1, 2, 3 };
+
+            ArrayImplementations.ResizeArray<int>(ref arr, 10);
+
+            string arrString = string.Join(", ", arr);
+            Console.WriteLine(arrString);
+
+            int item = ArrayImplementations.GetAt<int>(arr, 2, sizeof(int));
+            Console.WriteLine(item);
+            Console.WriteLine(arr[2]);
+        }
+    }
+}
